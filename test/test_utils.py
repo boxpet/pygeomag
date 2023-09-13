8f2aacbabd0b287e7055623ffec7ddffada72016
Justin Myers <justmobilize@users.noreply.github.com>
2023-09-13 08:50:35 -0700

    Update to version 0.2.0

--- test/test_utils.py
+++ test/test_utils.py
@@ -5,4 +5,4 @@ from pygeomag import __version__
 
 class TestUtil(TestCase):
     def test_version(self):
-        self.assertEqual(__version__, "0.1.0")
+        self.assertEqual(__version__, "0.2.0")
